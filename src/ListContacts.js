09e9e86a08446ca607ccacd8f499a92be6f773f0
Kaushal Patel <kaushalpatel922@gmail.com>
2017-07-10 10:51:19 -0400

    sorting search result

--- src/ListContacts.js
+++ src/ListContacts.js
@@ -1,9 +1,11 @@
 import React, {Component} from 'react'
 import PropTypes from 'prop-types'
+import escapeRegExp from 'escape-string-regexp'
+import sortBy from 'sort-by'
 
 class ListContacts extends Component {
   static propTypes = {
-    contats: PropTypes.array.isRequired,
+    contacts: PropTypes.array.isRequired,
     onDeleteContact: PropTypes.func.isRequired
   }
 
@@ -14,9 +16,18 @@ class ListContacts extends Component {
     this.setState({ query: query.trim() })
   }
   render () {
+    let showingContacts;
+    if(this.state.query) {
+      const match = new RegExp(escapeRegExp(this.state.query), 'i')
+      showingContacts = this.props.passingContacts.filter((contact) => match.test(contact.name))
+    } else {
+      showingContacts = this.props.passingContacts
+    }
+
+    showingContacts.sort(sortBy('name'))
+
     return (
       <div className='list-contacts'>
-      {JSON.stringify(this.state)}
         <div className='list-contacts-top'>
           <input
             className='search-contacts'
@@ -27,7 +38,7 @@ class ListContacts extends Component {
           />
         </div>
         <ol className='contact-list'>
-          {this.props.passingContacts.map((contact) => (
+          {showingContacts.map((contact) => (
             <li key={contact.id} className='contact-list-item'>
               <div className='contact-avatar' style ={{
                 backgroundImage: `url(${contact.avatarURL})`
